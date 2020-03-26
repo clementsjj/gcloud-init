1f35124c33dbbf04827207750999954d8c4fa262
JJ Clements <clementsjj@gmail.com>
2020-03-26 18:21:53 -0400

    Create quickstart.sh

---
+++ gcp/quickstart.sh
@@ -0,0 +1,10 @@
+#!bin/bash
+
+bash ./scripts/gcloud-additional-components.sh
+bash ./scripts/enableapis.sh
+bash ./scripts/network.sh
+bash ./scripts/privatebucket.sh
+bash ./scripts/publicbucket.sh
+bash ./scripts/pubsub.sh
+bash ./scripts/bigquery.sh
+bash ./scripts/bigtable.sh

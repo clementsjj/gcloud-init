c60e8a7db4ae44c4a1fae02b4b5ab3983b3c99d9
JJ Clements <clementsjj@gmail.com>
2020-03-26 18:30:30 -0400

    Update quickstart.sh

--- gcp/quickstart.sh
+++ gcp/quickstart.sh
@@ -12,22 +12,22 @@ echo *** Creating Network ***
 bash ./scripts/network.sh
 echo ~~finished creating network~~
 
-echo *** Creating Private Bucket ***
-bash ./scripts/privatebucket.sh
-echo ~~finished private bucket~~
+# echo *** Creating Private Bucket ***
+# bash ./scripts/privatebucket.sh
+# echo ~~finished private bucket~~
 
-echo *** Creating Public Bucket ***
-bash ./scripts/publicbucket.sh
-echo ~~finished public bucket~~
+# echo *** Creating Public Bucket ***
+# bash ./scripts/publicbucket.sh
+# echo ~~finished public bucket~~
 
-echo *** Setting Up PubSub ***
-bash ./scripts/pubsub.sh
-echo ~~finished pubsub~~
+# echo *** Setting Up PubSub ***
+# bash ./scripts/pubsub.sh
+# echo ~~finished pubsub~~
 
-echo *** Configuring Big Query ***
-bash ./scripts/bigquery.sh
-echo ~~finished bigquery~~
+# echo *** Configuring Big Query ***
+# bash ./scripts/bigquery.sh
+# echo ~~finished bigquery~~
 
-echo *** Configuring Big Table ***
-bash ./scripts/bigtable.sh
-echo ~~finished bigtable~~
+# echo *** Configuring Big Table ***
+# bash ./scripts/bigtable.sh
+# echo ~~finished bigtable~~
